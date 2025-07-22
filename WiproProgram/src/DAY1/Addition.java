7d974d6234737beefb0040de4c01b713ab740e60
Dineesha <Dineesha@LAPTOP-15G4U92L>
2025-07-22 17:49:26 +0530

    Initial Commit

---
+++ WiproProgram/src/DAY1/Addition.java
@@ -0,0 +1,12 @@
+package DAY1;
+
+public class Addition {
+		
+		 public static void main(String[] args) {
+		        int a = 5;
+		        int b = 7;
+		        int sum = a + b;
+		        System.out.println("Sum = " + sum);
+		    }
+	}
+
